813701522b1dbad2be6ee9ac335cc3af7df30911
marcelloburattini79 <burattinimarcello@yahoo.it>
2020-03-24 12:03:08 +0100

    primi commit

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
+
 var app = new Vue({
   el: "#app",
   data: {
+
+    /*Achille: {
+      nome: "Achille",
+      totale: 7,
+      punteggio: 0
+    },
+
+     Ettore: {
+      nome: "Ettore",
+      totale: 21,
+      punteggio: 0
+    },
+
+    Paride: {
+      nome: "Paride",
+      totale: 14,
+      punteggio: 0
+    },
+
+    Aiace: {
+      nome: "Aiace",
+      totale: 3,
+      punteggio: 0
+    },
+
+    giocatori: [Achille, Ettore, Paride, Aiace] */
+
+    secondi: 0
     
   },
+  created(){
+    setInterval(()=>{
+      this.secondi++;
+    }, 1000);
+  }
  
 });
 
